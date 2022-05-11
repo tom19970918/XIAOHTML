47a5593efcbadd460be007cfdfc21d56a0630cd3
tom19970918 <tom19970918@gmail.com>
2022-05-11 15:30:59 +0800

    調整LOGO+name_JQ+header目錄

---
+++ js/all.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$('.name').hover(function () {
+			// over
+			$(".name h1").css("font-size","24px");
+			$(".name p").css("font-size","46px");
+		}, function () {
+			// out
+			$(".name h1").css("font-size","46px");
+			$(".name p").css("font-size","24px");
+	});
+});
