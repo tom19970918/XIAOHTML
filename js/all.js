3bce3bd2fca50f526d2fafdbb8f85aba2daec563
tom19970918 <tom19970918@gmail.com>
2022-05-12 13:09:34 +0800

    新增:漢堡頁

--- js/all.js
+++ js/all.js
@@ -8,4 +8,9 @@ $(document).ready(function () {
 			$(".name h1").css("font-size","46px");
 			$(".name p").css("font-size","24px");
 	});
+
+	$('.show_menu_2').click(function (e) { 
+		e.preventDefault();
+		$('.menu_2').toggleClass('active');
+	});
 });
